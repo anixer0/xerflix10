98b49e3bf558d829aa0e822de04baea6a4921403
anixer0 <anixer42@gmail.com>
2024-07-08 18:09:58 +0800

    Update videoplayer.tsx

--- components/movie/containers/videoplayer.tsx
+++ components/movie/containers/videoplayer.tsx
@@ -4,16 +4,14 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function VideoPlayer({ id }:any) {
   return (
     <div className="pb-8">
-      <Tabs defaultValue="vidsrcvip">
+      <Tabs defaultValue="vidsrc">
         <div className="flex flex-col items-center">
           <TabsList>
             <TabsTrigger value="vidsrc">VidSrc</TabsTrigger>
-            <TabsTrigger value="XerS1">XerS1</TabsTrigger>
-            <TabsTrigger value="vidsrcvip">vidSrcvip</TabsTrigger>
+            <TabsTrigger value="vidsrcvip">VidSrc.Vip</TabsTrigger>
             <TabsTrigger value="vidsrcpro">Vidsrc.pro</TabsTrigger>
             <TabsTrigger value="vidsrcin">Vid.In</TabsTrigger>
             <TabsTrigger value="superembed">SuperEmbed</TabsTrigger>
-            <TabsTrigger value="123embed">123embed</TabsTrigger>
           </TabsList>
         </div>
         <TabsContent value="vidsrc">
@@ -30,28 +28,6 @@ export default function VideoPlayer({ id }:any) {
         <TabsContent value="vidsrcvip">
           <iframe
             src={`https://vidsrc.vip/embed/movie/${id}`}
-            referrerPolicy="origin"
-            allowFullScreen
-            width="100%"
-            height="450"
-            scrolling="no"
-            className="max-w-3xl mx-auto px-4 pt-10"
-          ></iframe>
-          </TabsContent>
-          <TabsContent value="123embed">
-          <iframe
-            src={`https://play2.123embed.net/movie/${id}`}
-            referrerPolicy="origin"
-            allowFullScreen
-            width="100%"
-            height="450"
-            scrolling="no"
-            className="max-w-3xl mx-auto px-4 pt-10"
-          ></iframe>
-          </TabsContent>
-          <TabsContent value="XerS1">
-          <iframe
-            src={`https://player.autoembed.cc/embed/movie/${e}?server=2`}
             referrerPolicy="origin"
             allowFullScreen
             width="100%"
